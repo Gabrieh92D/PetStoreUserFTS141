b969885cfee1c6b86943591868c9e72df6b9184f
“Gabriel <gabriel.marques.montagnoli@gmail.com>
2024-09-04 21:18:29 -0300

    PetStoreUserFTS141

---
+++ fixtures/userMassa.js
@@ -0,0 +1,42 @@
+module.exports.array = [
+  {
+    "id": 297537501,
+    "username": "Carlos99",
+    "firstName": "Carlos",
+    "lastName": "Silva",
+    "email": "carlos.silva@gmail.com",
+    "password": "carl@99",
+    "phone": "11987654321",
+    "userStatus": 0
+  },
+  {
+    "id": 297537502,
+    "username": "Maria88",
+    "firstName": "Maria",
+    "lastName": "Oliveira",
+    "email": "maria.oliveira@gmail.com",
+    "password": "mari@88",
+    "phone": "21998765432",
+    "userStatus": 0
+  },
+  {
+    "id": 297537503,
+    "username": "Joao123",
+    "firstName": "João",
+    "lastName": "Souza",
+    "email": "joao.souza@gmail.com",
+    "password": "jo@o123",
+    "phone": "31912345678",
+    "userStatus": 0
+  },
+  {
+    "id": 297537504,
+    "username": "Ana456",
+    "firstName": "Ana",
+    "lastName": "Pereira",
+    "email": "ana.pereira@gmail.com",
+    "password": "ana@456",
+    "phone": "41934567890",
+    "userStatus": 0
+  }
+]
